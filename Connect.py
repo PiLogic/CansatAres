6b8a7f329e33ba4a64e7e4311560670cafc41e2e
PiLogic <isaaccollis11@icloud.com>
2025-01-27 20:42:04 +0000

    Connection Window

---
+++ Connect.py
@@ -0,0 +1,105 @@
+from tkinter import *
+from tkinter import ttk
+
+class ConnectWindow:
+
+    def __init__(self):
+
+        self.root = Tk()
+
+        self.root.geometry('800x500')
+
+        self.root.title('Connection Status')
+
+        self.background = '#142536'
+        
+        self.root.config(background = self.background)
+
+        self.main()
+        
+        self.root.mainloop()
+
+    def main(self):
+        self.sensors = [('Wind Sens.','Rev.C',True),('Humidity, Pressure & Temp Sens.', 'BME280',True),('Air Quality Sens.','CCS811',False),('CO & Gas Sens.','MiCS5524',False)]
+        self.statusUI()
+        print('bob')
+
+    def statusUI(self):
+ 
+        self.title = Label(self.root,
+                      text = 'Connection Status',
+                      fg = 'White',
+                      bg = self.background,
+                      font = ('Quicksand',50,'bold'))
+        self.title.place(x = 170, y = 35)
+
+        self.linkText = Label(self.root,
+                      text = 'Linked to CanSat',
+                      fg = 'White',
+                      bg = self.background,
+                      font = ('Quicksand',30,'normal'))
+        self.linkText.place(x = 50, y = 150)
+
+        self.linkStatus = Label(self.root,
+                      text = '✅',
+                      fg = 'Green',
+                      bg = self.background,
+                      font = ('Quicksand',30,'normal'))
+        self.linkStatus.place(x = 700, y = 150)
+
+        self.sensorTextOnes = []
+        self.sensorTextTwos = []
+        self.sensorStatuses = []
+
+        for i, sensor in enumerate(self.sensors):
+            self.sensorTextOnes.append(Label(self.root,
+                      text = sensor[0],
+                      fg = 'White',
+                      bg = self.background,
+                      font = ('Quicksand',30,'normal')))
+            self.sensorTextTwos.append(Label(self.root,
+                      text = sensor[1],
+                      fg = 'White',
+                      bg = self.background,
+                      font = ('Quicksand',30,'normal')))
+            self.sensorStatuses.append(Label(self.root,
+                      text = '✅' if sensor[2] else '🔄',
+                      fg = 'Orange',
+                      bg = self.background,
+                      font = ('Quicksand',30,'normal')))
+            
+            gap = 60
+
+            self.sensorTextOnes[-1].place(x = 50, y = 150 + (i+1)*gap)
+            self.sensorTextTwos[-1].place(x = 535, y = 150 + (i+1)*gap)
+            self.sensorStatuses[-1].place(x = 700, y = 150 + (i+1)*gap)
+
+
+
+
+
+    def optionPress(self, value):
+        if value == 'Connect':
+            self.connect()
+        elif value == 'Troubleshoot':
+            self.troubleshoot()
+        elif value == 'Begin':
+            self.begin()
+        else:
+            print('Error 1 - Unknown Option')
+
+    def troubleshoot(self):
+        print('Troubleshooting...')
+
+    def connect(self):
+        print('Connecting...')
+
+    def begin(self):
+        print('Beginning...')
+        self.root.destroy()
+        self.running = Running()
+        
+
+if __name__ == '__main__':
+    application = ConnectWindow()
+ 
